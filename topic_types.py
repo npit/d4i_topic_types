9cae2b4cfe1a9eb4a2eea4b66e1b1d839ebdb4b1
npit <pittarasnikif@iit.demokritos.gr>
2019-09-10 21:02:49 +0300

    Fix hibernatings

--- topic_types.py
+++ topic_types.py
@@ -1,22 +1,80 @@
 import sys
 
+import matplotlib.pylab as plt
 import numpy as np
 import pandas
+import seaborn as sns
 
 
-def check_safe_bet(topic_info, years_weights_stats, num_years=10):
+def check_safe_bet(topic_info, years_weights_stats, num_years=None, stdev_w_coeff=1):
     sorted_years = sorted(topic_info["years"].keys(), reverse=True)
     if num_years is not None:
         sorted_years = sorted_years[:num_years]
     for year in sorted_years:
         weight = topic_info["years"][year]
         ymean, ystd = years_weights_stats[year]["mean"], years_weights_stats[year]["std"]
+        ystd = ystd * stdev_w_coeff
         if weight < (ymean + ystd):
             # print("Topic not safe bet due to w {} on year {} with avg + std: {}".format(weight, year, ymean + ystd))
             return False
     return True
 
-def check_hibernating(topic_info, years_weights_stats, min_g_years=None, policy="full"):
+
+
+
+def check_hibernating(topic_info, years_weights_stats, min_g_years=None, policy="full", stdev_w_coeff=1, valid_range=None, do_visualize=False):
+    # find years where only one of the conditions exist
+    # then find continuous sequences
+    giant_threshold, hib_threshold = {}, {}
+    sorted_years = sorted(topic_info["years"].keys(), reverse=True)
+    year_means, year_flucts = {}, {}
+
+    for year in sorted_years:
+        weight = topic_info["years"][year]
+        ymean, ystd = years_weights_stats[year]["mean"], years_weights_stats[year]["std"]
+        ystd = ystd * stdev_w_coeff
+        fluctuation_term = ystd
+        giant_threshold[year], hib_threshold[year] = ymean - fluctuation_term, ymean + fluctuation_term
+        year_means[year] = ymean
+        year_flucts[year] = fluctuation_term
+
+    if any(x<0 for x in giant_threshold.values()):
+        print("Negative value in giant threshold{} both as giant and hibernating. Curr stdev coeff is {}".format(year, stdev_w_coeff))
+        exit(1)
+
+    gyears, hyears = [], []
+    for year in sorted_years:
+        weight = topic_info["years"][year]
+        if weight > giant_threshold[year]:
+            gyears.append(year)
+        if weight < hib_threshold[year]:
+            hyears.append(year)
+
+    # if both giant and hibernating, the stdw coeff needs tuning
+    for year in sorted_years:
+        if year in gyears and year in hyears:
+            # remove year in both categories
+            gyears.remove(year)
+            hyears.remove(year)
+
+    gyears.reverse()
+    hyears.reverse()
+    if not gyears or not hyears:
+        return None
+
+    # check "edge regions", make sure that latest is hibernating and earliest is giant
+    if not(min(gyears) < max(hyears)):
+        return None
+
+    if valid_range is None or (gyears[-1] in valid_range or hyears[0] in valid_range):
+        # good!
+        if do_visualize is not None:
+            visualize_hibernating(hyears, gyears, giant_threshold, hib_threshold, topic_info, output=do_visualize)
+        return gyears, hyears
+    return None
+
+def check_hibernating_flawed(topic_info, years_weights_stats, min_g_years=None, policy="full", stdev_w_coeff=1):
+    """This function does not check for overlapping conditions -- it's crap"""
     sorted_years = sorted(topic_info["years"].keys(), reverse=True)
     checking_hibernation = True
     hyears, gyears = [], []
@@ -33,17 +91,22 @@ def check_hibernating(topic_info, years_weights_stats, min_g_years=None, policy=
     # above just for visualization / inverstigation
 
     index = 0
+    # consideration "limits" per year
+    giant_threshold, hib_threshold = {}, {}
     while index < len(sorted_years):
         year = sorted_years[index]
         weight = topic_info["years"][year]
         ymean, ystd = years_weights_stats[year]["mean"], years_weights_stats[year]["std"]
+        ystd = ystd * stdev_w_coeff
+        giant_threshold[year], hib_threshold[year] = ymean - ystd, ymean + ystd
+
         if checking_hibernation:
             if weight < (ymean + ystd):
                 hyears.append(year)
                 hl[index] = "H"
             else:
                 if not hyears:
-                    return False
+                    return None
                 checking_hibernation = False
                 index -= 1
         else:
@@ -62,9 +125,12 @@ def check_hibernating(topic_info, years_weights_stats, min_g_years=None, policy=
     if policy == "full":
         if (hyears + gyears) != sorted_years:
             return None
+    gyears.reverse()
+    hyears.reverse()
+    visualize_hibernating(hyears, gyears, giant_threshold, hib_threshold,  topic_info)
     return (gyears, hyears)
 
-def check_emerging(topic_info, years_w_stats, years_g_stats, num_low_weight=None):
+def check_emerging(topic_info, years_w_stats, years_g_stats, num_low_weight=None, stdev_w_coeff=1, stdev_gr_coeff=1):
     # check a range with low topic weight wrt year average, if any
     low_weight_years = []
     high_growth_years = []
@@ -73,7 +139,7 @@ def check_emerging(topic_info, years_w_stats, years_g_stats, num_low_weight=None
     for year in sorted_years:
         topic_growth = topic_info["growths"][year]
         year_mean_growth = years_g_stats[year]['mean']
-        year_std_growth = years_g_stats[year]['std']
+        year_std_growth = years_g_stats[year]['std'] * stdev_gr_coeff
 
         if topic_growth > (year_mean_growth + year_std_growth):
             high_growth_years.append(year)
@@ -88,7 +154,7 @@ def check_emerging(topic_info, years_w_stats, years_g_stats, num_low_weight=None
             continue
         topic_weight = topic_info["years"][year]
         year_mean_weight = years_w_stats[year]['mean']
-        year_std_weight = years_w_stats[year]['std']
+        year_std_weight = years_w_stats[year]['std'] * stdev_w_coeff
 
         if topic_weight < (year_mean_weight - year_std_weight):
             low_weight_years.append(year)
@@ -104,6 +170,8 @@ def check_emerging(topic_info, years_w_stats, years_g_stats, num_low_weight=None
         # low weight years has to be enough
         if len(low_weight_years) < num_low_weight:
             return None
+    low_weight_years.reverse()
+    high_growth_years.reverse()
     return low_weight_years, high_growth_years
 
 def main():
@@ -111,11 +179,18 @@ def main():
     # topicid 	topicname 	year 	weight
     try:
         infile = sys.argv[1]
+        exclusion = sys.argv[1]
     except:
         infile = "topic_trends_per_year.csv"
+        exclusion = [7, 15, 51, 77, 78, 86, 150, 153, 158, 160, 164, 201, 225, 230, 244, 253, 259, 271, 273, 285, 286, 341, 342, 390, 426, 446, 466, 472, 475, 477, 493]
+
+    print("Reading from ", infile)
+    print("Excluding ", len(exclusion), " topics")
 
     x = pandas.read_csv(infile)
-    x = x[x["year"] < 2020]
+    x = x[x["year"] < 2019]
+    if exclusion:
+        x = x[~x["topicid"].isin(exclusion)]
     topics = {}
     years_weights = {}
     years_growths = {}
@@ -133,6 +208,7 @@ def main():
        if topic not in topics:
            topics[topic] = {"years":{}}
        topics[topic]["label"] = topicname
+       topics[topic]["index"] = topic
        topics[topic]["years"][year] = weight
        if year not in years_weights:
           years_weights[year] = []
@@ -167,6 +243,9 @@ def main():
        years_weights_stats[year]["mean"], years_weights_stats[year]["std"] = np.mean(w) if w else None, np.std(w) if w else None
        years_growths_stats[year]["mean"], years_growths_stats[year]["std"] = np.mean(gr) if gr else None, np.std(gr) if gr else None
 
+    print("Year-wise statistics means")
+    for year in years_weights_stats:
+        print(year, years_weights_stats[year]['mean'], years_weights_stats[year]['std'])
 
     # decide topic types
     emergings, safes, hibernating = [], [], []
@@ -176,39 +255,55 @@ def main():
         topic_info["types"] = []
 
         num_low_weight = None
-        res = check_emerging(topic_info, years_weights_stats, years_growths_stats, num_low_weight)
+        em_valid_range = [ 2015, 2016, 2017, 2018, 2019]
+        # res = check_emerging(topic_info, years_weights_stats, years_growths_stats, num_low_weight,stdev_w_coeff=0)
+        res = check_emerging(topic_info, years_weights_stats, years_growths_stats, num_low_weight, stdev_w_coeff=0.5)
         if res is not None:
-            emergings.append(t)
-            topic_info["types"].append("E")
-        num_safe_years = 10
+            low, high = res
+            if not em_valid_range or (low[-1] in em_valid_range or high[0] in em_valid_range):
+                emergings.append((t, res))
+                topic_info["types"].append("E")
+        num_safe_years = None
         if check_safe_bet(topic_info, years_weights_stats, num_years=num_safe_years):
             safes.append(t)
             topic_info["types"].append("S")
 
         min_g_years = None
         policy="full"
-        if check_hibernating(topic_info, years_weights_stats, min_g_years,policy):
-            hibernating.append(t)
+        hi_valid_range = list(range(2011, 2013+1))
+        coeff = 0.5
+        visualize = None
+        visualize = "show" # "write"
+        visualize = "write" 
+        g_h_years = check_hibernating(topic_info, years_weights_stats, min_g_years,policy, stdev_w_coeff=coeff, valid_range=hi_valid_range, do_visualize=visualize)
+        if g_h_years is not None:
+            hibernating.append((t, g_h_years))
             topic_info["types"].append("H")
 
-    print("Emerging: num low weight years {}".format(num_low_weight))
-    for i, e in enumerate(emergings):
-        print("{}/{} : {} {}".format(i+1, len(emergings), e, topics[e]["label"]))
-        if i > 8:
-            print("...")
-            break
+    print("Emerging: num low weight years {}, range: {}".format(num_low_weight, em_valid_range))
+    for i, e in enumerate(sorted(emergings)):
+        topic = e[0]
+        last_low = e[1][0][-1]
+        print("{}/{} |last low year: {}|: {} {}".format(i+1, len(emergings), last_low, topic, topics[topic]["label"]))
+        # if i > 8:
+        #     print("...")
+        #     break
     print("Safe: num safe years: {}".format(num_safe_years))
-    for i, e in enumerate(safes):
+    for i, e in enumerate(sorted(safes)):
         print("{}/{} : {} {}".format(i+1, len(safes), e, topics[e]["label"]))
-        if i > 8:
-            print("...")
-            break
-    print("Hibernating: min giant years: {}, span policy: {}".format(min_g_years, policy))
-    for i, e in enumerate(hibernating):
-        print("{}/{} : {} {}".format(i+1, len(hibernating), e, topics[e]["label"]))
-        if i > 8:
-            print("...")
-            break
+        # if i > 8:
+        #     print("...")
+        #     break
+    print("Hibernating: min giant years: {}, span policy: {}, range: {}".format(min_g_years, policy, hi_valid_range))
+    for i, e in enumerate(sorted(hibernating, key=lambda x: x[0])):
+        topic, gh_years = e
+        giants, hibers = gh_years
+        lastgyear, firsthyear = giants[-1], hibers[0]
+        print("{}/{} | last giant / first hib years: {}, {} | : {} {}".format(i+1, len(hibernating), lastgyear, firsthyear,
+              topic, topics[topic]["label"]))
+        # if i > 8:
+        #     print("...")
+        #     break
     print("Got {} emerging topics, {} hibernating: and {} safe bets".format(
         len(emergings), len(hibernating), len(safes)))
 
@@ -243,15 +338,100 @@ def main():
     x["emerging"] = emcol
     x["safebet"] = safecol
 
-    x.to_csv("expanded_cols.csv")
+    return
+
+    x.to_csv("topic_diffs_growths_types.filteredQuality.csv")
 #    print("\nmax:\n", x.max())
 #    print("\nmin:\n", x.min())
 #    print("\nsum:\n", x.sum())
 
-
     # write one without the per-year information
     y = x[["topicid","topicname","hibernating","emerging","safebet"]].drop_duplicates()
-    y.to_csv("expaned_cols_onlytypes.csv", index=None)
+    y.to_csv("topic_diffs_growths_types.filteredQuality.onlytypes.csv", index=None)
+
+
+    # plot
+    safes = x[x["safebet"] ==1]
+    sortedyears = sorted(years_weights.keys())
+    all_weights = np.ndarray((0, len(sortedyears)), dtype=np.float32)
+    topicnames = []
+    for topic in set(list(safes["topicid"])):
+        topicnames.append(topics[topic]['label'])
+        weights = [0 for _ in sortedyears]
+        for year in topics[topic]['years']:
+            weights[sortedyears.index(year)] = topics[topic]['years'][year]
+        all_weights = np.vstack((all_weights, np.asarray(weights)))
+    fix, ax = plt.subplots()
+    ax.stackplot(safes["year"].drop_duplicates().sort_values(), *all_weights)
+    plt.title("Safe bets")
+    plt.legend(topicnames)
+    plt.show()
+
+
+def visualize_hibernating(hyears, gyears, giant_threshold, hib_threshold, topic_info, output="show"):
+    plt.figure()
+    topic_index = topic_info["index"]
+    name = topic_info["label"]
+    topic_name = "{}: {}".format(topic_index, name)
+    print("Topic ", topic_name)
+
+    weights = topic_info["years"]
+    years = sorted(list(giant_threshold.keys()))
+    xidx = list(range(len(years)))
+    # giant lower limit
+    plt.plot(xidx, [giant_threshold[years[i]] for i in xidx],'r')
+    # hibernating upper limit
+    plt.plot(xidx, [hib_threshold[years[i]] for i in xidx],'g')
+    # year mean
+    # plt.plot(xidx, [year_means[year] for year in years],'--')
+    # year fluctuation
+    # plt.plot(xidx, [year_flucts[year] for year in years],'.')
+    # print("Fluctuations:", [year_flucts[year] for year in years])
+    # print("Means:", [year_means[year] for year in years])
+    # leg = ["giant_year_thr","hib_year_thr", "year_mean", "year_fluct"]
+    # leg = ["giant_year_thr","hib_year_thr", "year_mean"]
+    leg = ["giant_year_thr","hib_year_thr"]
+
+
+    topic_g, topic_h, topic_neutral = [],[],[]
+    for i, year in enumerate(years):
+        if year not in weights:
+            continue
+        weight = weights[year]
+        if year in gyears:
+            topic_g.append((i, weight))
+        elif year in hyears:
+            topic_h.append((i, weight))
+        else:
+            topic_neutral.append((i, weight))
+    if topic_g:
+        plt.plot(*zip(*topic_g), "r*")
+        if "topic giant" not in leg:
+            leg.append("topic giant")
+    if topic_h:
+        plt.plot(*zip(*topic_h), "g*")
+        if "topic hib" not in leg:
+            leg.append("topic hib")
+    if topic_neutral:
+        plt.plot(*zip(*topic_neutral), "ko")
+        if "topic neutral" not in leg:
+            leg.append("topic neutral")
+
+    plt.legend(leg)
+    plt.title(topic_name)
+    plt.xticks(ticks=range(len(years)), labels=years, rotation='vertical')
+    if output == "show":
+        plt.show()
+    elif output == "write":
+        outpath = topic_name.replace("/","_") + ".png"
+        print("Writing to", outpath)
+        plt.savefig(outpath)
+    else:
+        print("Visualization", output, "undefined")
+        exit(1)
+
+    print()
+
 
 if __name__ == '__main__':
     main()
